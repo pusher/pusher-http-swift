2b3ec47d33ab06555717c8523679f80dae7405fa
Daniel Browne <daniel.browne@pusher.com>
2021-04-26 16:10:00 +0100

    Add 'ChannelAttributable' protocol

--- Sources/Pusher/Protocols/ChannelInfoRecord.swift
+++ Sources/Pusher/Protocols/ChannelInfoRecord.swift
@@ -1,10 +1,8 @@
 import Foundation
 
 /// An information record for a specific channel.
-protocol ChannelInfoRecord {
+protocol ChannelInfoRecord: ChannelAttributable {
 
     /// If there is at least one user subscribed to the channel, it is classed as occupied.
     var isOccupied: Bool { get }
-
-    var attributes: ChannelAttributes { get }
 }
--- Sources/Pusher/Protocols/ChannelSummaryRecord.swift
+++ Sources/Pusher/Protocols/ChannelSummaryRecord.swift
@@ -1,12 +1,10 @@
 import Foundation
 
 /// A summarised information record for a specific channel.
-protocol ChannelSummaryRecord {
+protocol ChannelSummaryRecord: ChannelAttributable {
 
     /// The channel name.
     var name: String { get }
-
-    var attributes: ChannelAttributes { get }
 
     /// The channel type.
     var type: ChannelType { get }

e5f04b2a9d04de170dde53b28a64c85e68a8564d
Daniel Browne <daniel.browne@pusher.com>
2021-05-05 16:46:26 +0100

    Add Quick Help documentation to main public API methods

--- Sources/Pusher/Pusher.swift
+++ Sources/Pusher/Pusher.swift
@@ -31,6 +31,16 @@ public class Pusher {
 
     // MARK: - Application state queries
 
+    /// Fetch a list of any occupied channels.
+    ///
+    /// The results can be filtered by channel type, or a user-specified prefix
+    /// that allows filtering based on custom naming schemes.
+    /// - Parameters:
+    ///   - filter: A filter to apply to the returned results.
+    ///   - attributeOptions: A set of attributes that should be returned for each result.
+    ///   - callback: A closure that returns a `Result` containing a list of occupied
+    ///               channels (and their requested attributes), or a `PusherError`
+    ///               if the operation fails for some reason.
     public func channels(withFilter filter: ChannelFilter = .any,
                          attributeOptions: ChannelAttributeFetchOptions = [],
                          callback: @escaping (Result<[ChannelSummary], PusherError>) -> Void) {
@@ -47,6 +57,12 @@ public class Pusher {
         }
     }
 
+    /// Fetch attributes of a given occupied channel.
+    /// - Parameters:
+    ///   - channel: The channel to inspect.
+    ///   - attributeOptions: A set of attributes that should be returned for the `channel`.
+    ///   - callback: A closure that returns a `Result` containing the requested information
+    ///               for the `channel`, or a `PusherError` if the operation fails for some reason.
     public func channelInfo(for channel: Channel,
                             attributeOptions: ChannelAttributeFetchOptions = [],
                             callback: @escaping (Result<ChannelInfo, PusherError>) -> Void) {
@@ -60,6 +76,14 @@ public class Pusher {
         }
     }
 
+    /// Fetch a list of users currently subscribed to a given occupied presence channel.
+    ///
+    /// Attempting to fetch a list of users for non-presence channels is invalid and
+    /// will result in an error.
+    /// - Parameters:
+    ///   - channel: The presence channel to inspect.
+    ///   - callback: A closure that returns a `Result` containing the subscribed users
+    ///               for the `channel`, or a `PusherError` if the operation fails for some reason.
     public func users(for channel: Channel,
                       callback: @escaping (Result<[User], PusherError>) -> Void) {
 
@@ -74,6 +98,17 @@ public class Pusher {
         }
     }
 
+    /// Trigger an event on one or more channels.
+    ///
+    /// The channel (or channels) that the event should be triggered on, (as well as the
+    /// attributes to fetch for the each channel) are specified when initializing an instance
+    /// of `Event` to pass to this method.
+    /// - Parameters:
+    ///   - event: The event to trigger.
+    ///   - callback: A closure that returns a `Result` containing a list of channels that
+    ///               were triggered to (and their requested attributes), or a `PusherError`
+    ///               if the operation fails for some reason. If the `attributeOptions` on
+    ///               the `event` are not set, an empty channel list will be returned.
     public func trigger(event: Event,
                         callback: @escaping (Result<[ChannelSummary], PusherError>) -> Void) {
 
@@ -93,6 +128,20 @@ public class Pusher {
         }
     }
 
+    /// Trigger multiple events, each on a specific channel.
+    ///
+    /// The channel that the event should be triggered on, (as well as the
+    /// attributes to fetch for the each channel) are specified when initializing instances
+    /// of `Event` to pass to this method.
+    ///
+    /// Specifying more than one channel to trigger the event on (or providing a list of
+    /// more than 10 events to trigger) is invalid and will result in an error.
+    /// - Parameters:
+    ///   - events: A list of events to trigger.
+    ///   - callback: A closure that returns a `Result` containing the requested information
+    ///               for the channels that were triggered to, or a `PusherError`
+    ///               if the operation fails for some reason. If the `attributeOptions` on
+    ///               the `event` are not set, an empty information list will be returned.
     public func trigger(events: [Event],
                         callback: @escaping (Result<[ChannelInfo], PusherError>) -> Void) {
 
@@ -113,6 +162,16 @@ public class Pusher {
 
     // MARK: - Webhook verification
 
+    /// Verify that a received webhook request is genuine and was received from Pusher.
+    ///
+    /// Since a webhook endpoint is accessible to the global internet, verifying that webhook request
+    /// originated from Pusher is important. Valid webhooks contain special headers which contain a
+    /// copy of your application key and a HMAC signature of the webhook payload (i.e. its body).
+    /// - Parameters:
+    ///   - request: The webhook request.
+    ///   - callback: A closure that returns a `Result` containing a verified `Webhook` and the
+    ///               events that it contains (which are decrypted if needed), or a `PusherError`
+    ///               if the operation fails for some reason.
     public func verifyWebhookRequest(_ request: URLRequest,
                                      callback: @escaping (Result<Webhook, PusherError>) -> Void) {
 
@@ -129,6 +188,17 @@ public class Pusher {
 
     // MARK: - Private and presence channel subscription authentication
 
+    /// Generate an authentication token that can be returned to a user client that is attempting
+    /// to subscribe to a private or presence channel, which requires authentication with the server.
+    /// - Parameters:
+    ///   - channel: The `Channel` for which to generate the authentication token.
+    ///   - socketId: The socket identifier `String` for the connected user.
+    ///   - userData: User data required when generating an authentication token for a subscription attempt
+    ///               to a presence channel.
+    ///               (This must be set when generating an authentication token for a presence channel).
+    ///   - callback: A closure that returns a `Result` containing an authentication token
+    ///               for subscribing to a private or presence channel, or a `PusherError`
+    ///               if the operation fails for some reason.
     public func authenticate(channel: Channel,
                              socketId: String,
                              userData: PresenceUserAuthData? = nil,

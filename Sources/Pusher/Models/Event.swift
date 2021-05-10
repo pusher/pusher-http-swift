013bdeb58c0404e208db688d2d621bbe3de67d51
Daniel Browne <daniel.browne@pusher.com>
2021-05-10 12:36:25 +0100

    Add 'Event.Error'
    - Refactors 'PusherError.invalidConfiguration(reason:)' -> 'Event.Error'

--- Sources/Pusher/Models/Event.swift
+++ Sources/Pusher/Models/Event.swift
@@ -3,6 +3,23 @@ import Foundation
 /// An event to trigger on a specific channel (or multiple channels).
 public struct Event: EventInfoRecord, Encodable {
 
+    /// An error generated during initialization.
+    enum Error: LocalizedError {
+
+        /// Encrypted channels cannot be triggered to when initializing a multichannel `Event`.
+        case encryptedChannelsInvalidWithMultichannelEvents
+
+        /// A localized human-readable description of the error.
+        public var errorDescription: String? {
+
+            switch self {
+            case .encryptedChannelsInvalidWithMultichannelEvents:
+                return NSLocalizedString("Encrypted channels cannot be used on multichannel events.",
+                                         comment: "'.encryptedChannelsInvalidWithMultichannelEvents' error text")
+            }
+        }
+    }
+
     /// The channels to which the event will be sent (if publishing to multiple channels).
     public let channels: [Channel]?
 
@@ -76,8 +93,7 @@ public struct Event: EventInfoRecord, Encodable {
         // (Triggering an event on multiple channels is not allowed if any are encrypted).
         let containsEncryptedChannels = channels.contains { $0.type == .encrypted }
         guard !containsEncryptedChannels else {
-            let reason = "Cannot trigger an event on multiple channels if any of them are encrypted."
-            throw PusherError.invalidConfiguration(reason: reason)
+            throw Error.encryptedChannelsInvalidWithMultichannelEvents
         }
 
         self.channel = nil
